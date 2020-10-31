92588b8874e3af078bccfd20a3df9a5e7518a9fc
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2020-10-31 16:15:05 +0530

    :sparkles: Add input data validation
    Add input data validation middleware

--- api/user/router.ts
+++ api/user/router.ts
@@ -1,10 +1,29 @@
+import * as yup from "yup";
 import { Request, Response, Router } from "express";
+import { validateRequest } from "../shared/middlewares/inputDataValidation";
 import { userSignup } from "./controller";
 
 let router = Router();
 
+const userSignUpSchema = yup.object({
+  name: yup.string().required().trim(),
+  email: yup.string().email().required().trim(),
+  mobile: yup.string().length(10).required(),
+  user_name: yup.string().required().trim(),
+  class: yup.string().required().trim(),
+  password: yup.string().required().trim(),
+  role: yup
+    .string()
+    .matches(/(student|teacher)/)
+    .required(),
+});
+
 export const userRegisterHandler = () => {
-  router.post("/signup", userSignupHandler);
+  router.post(
+    "/signup",
+    validateRequest("body", userSignUpSchema),
+    userSignupHandler
+  );
 
   return router;
 };
